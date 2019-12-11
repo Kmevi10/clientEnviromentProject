8bf50cd45ff42d50626edc1e413b296e2556b68e
Kmevi10 <carlosmesaviera@gmail.com>
2019-12-11 20:57:24 +0100

    busqueda

--- js/classes.js
+++ js/classes.js
@@ -1,107 +1,107 @@
-class Coche{
-    constructor(marca, modelo, anno, km, combustible, precio){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.anno = anno;
-        this.km = km;
-        this.combustible = combustible;
-        this.precio = precio;
-        this.votos = [];
-    }
-    // GET && SET
-    get getMarca(){
+class Coche {
+    constructor(marca, modelo, anno, km, combustible, precio) {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.anno = anno;
+            this.km = km;
+            this.combustible = combustible;
+            this.precio = precio;
+            this.votos = [];
+        }
+        // GET && SET
+    get getMarca() {
         return this.marca;
     }
-    get getModelo(){
+    get getModelo() {
         return this.modelo;
     }
-    get getAnno(){
+    get getAnno() {
         return this.anno;
     }
-    get getKm(){
+    get getKm() {
         return this.km;
     }
-    get getCombustible(){
+    get getCombustible() {
         return this.combustible;
     }
-    get getPrecio(){
+    get getPrecio() {
         return this.precio;
     }
-    get getVotos(){
+    get getVotos() {
         return this.votos;
     }
 
-    set setMarca(marca){
+    set setMarca(marca) {
         this.marca = marca;
     }
-    set setModelo(modelo){
+    set setModelo(modelo) {
         this.modelo = modelo;
     }
-    set setAnno(anno){
+    set setAnno(anno) {
         this.anno = anno;
     }
-    set setKm(km){
+    set setKm(km) {
         this.km = km;
     }
-    set setCombustible(combustible){
+    set setCombustible(combustible) {
         this.combustible = combustible;
     }
-    set setPrecio(precio){
+    set setPrecio(precio) {
         this.precio = precio;
     }
 
-    toString(){
-        return  "Marca: "+ this.marca +
-                " ,Modelo: "+ this.modelo + 
-                " ,Año: " + this.anno + 
-                " ,Kilometros: "+ this.km + 
-                " ,Combustible: " + this.combustible + 
-                " ,Precio: "+ this.precio;
+    toString() {
+        return "Marca: " + this.marca +
+            " ,Modelo: " + this.modelo +
+            " ,Año: " + this.anno +
+            " ,Kilometros: " + this.km +
+            " ,Combustible: " + this.combustible +
+            " ,Precio: " + this.precio;
     }
 }
-class Persona{
-    constructor(nombre, apellidos, telefono, ciudad ){
+class Persona {
+    constructor(nombre, apellidos, telefono, ciudad) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.telefono = telefono;
         this.ciudad = ciudad;
- 
+
     }
     listaCochesVotados = []
 
     // METODOS
 
-    get getENombre(){
+    get getENombre() {
         return this.nombre;
     }
-    get getApellidos(){
+    get getApellidos() {
         return this.apellidos;
     }
-    get getTelefono(){
+    get getTelefono() {
         return this.telefono;
     }
-    get getciudad(){
+    get getciudad() {
         return this.ciudad;
     }
-    set setNombre(nombre){
+    set setNombre(nombre) {
         this.nombre = nombre;
     }
-    set setApellidos(apellidos){
+    set setApellidos(apellidos) {
         this.apellidos = apellidos;
     }
-    set setTelefono(telefono){
+    set setTelefono(telefono) {
         this.telefono = telefono;
     }
-    set setCiudad(ciudad){
+    set setCiudad(ciudad) {
         this.ciudad = ciudad;
     }
-    toString(){
+    toString() {
         return "Nombre: " + this.nombre + ", Apellidos: " + this.apellidos + ", Teléfono: " + this.telefono + ", Ciudad: " + this.ciudad;
     }
 }
 
-class Promocion extends Coche{
-    constructor(descuento, fechaFin, coches){
+class Promocion extends Coche {
+    constructor(descuento, fechaFin, coches) {
         super();
         this.descuento = descuento;
         this.fechaFin = fechaFin;
@@ -109,60 +109,64 @@ class Promocion extends Coche{
     }
 
     //GET && SET
-    
-    get getDescuento(){
+
+    get getDescuento() {
         return this.descuento;
     }
-    get getFechaFin(){
+    get getFechaFin() {
         return this.fechaFin;
     }
-    get getCoches(){
+    get getCoches() {
         return this.coches;
     }
 
-    set setDescuento(descuento){
+    set setDescuento(descuento) {
         this.descuento = descuento;
     }
-    set setFechaFin(fechaFin){
+    set setFechaFin(fechaFin) {
         this.fechaFin = fechaFin;
     }
-    set setCoches(coches){
+    set setCoches(coches) {
         this.coches = coches;
     }
 
-    obtenerDescuento(){
-        return "Precio final: "+ this.precio - this.descuento;
+    obtenerDescuento() {
+        return "Precio final: " + this.precio - this.descuento;
     }
 }
 
 class Usuario {
-    constructor(nombre_usuario, tipo, email){
+    constructor(nombre_usuario, tipo, email, coches) {
         this.nombre_usuario = nombre_usuario;
         this.tipo = tipo;
         this.email = email;
+        this.coches = coches;
     }
 
     // METODOS
 
-    get getNombreUsuario(){
+    get getNombreUsuario() {
         return this.nombre_usuario;
     }
-    get getTipo(){
+    get getTipo() {
         return this.tipo;
     }
-    get getEmail(){
+    get getEmail() {
         return this.email;
     }
-    set setNombreUsuario(nombre_usuario){
+    get getCoches() {
+        return this.coches;
+    }
+    set setNombreUsuario(nombre_usuario) {
         this.nombre_usuario = nombre_usuario;
     }
-    set setTipo(tipo){
+    set setTipo(tipo) {
         this.tipo = tipo;
     }
-    set setEmail(email){
+    set setEmail(email) {
         this.email = email;
     }
-    toString(){
+    toString() {
         return "Nombre de usuario: " + this.nombre_usuario + ", Tipo: " + this.tipo + ", Email: " + this.email;
     }
 }
@@ -170,7 +174,7 @@ class Usuario {
 
 
 class Pedido {
-    constructor(usuario, referencia, fecha, precio){
+    constructor(usuario, referencia, fecha, precio) {
         this.usuario = usuario;
         this.referencia = referencia;
         this.fecha = fecha;
@@ -179,26 +183,26 @@ class Pedido {
 
     // METODOS
 
-    get getReferencia(){
+    get getReferencia() {
         return this.referencia;
     }
-    get getFecha(){
+    get getFecha() {
         return this.fecha;
     }
-    get getPrecio(){
+    get getPrecio() {
         return this.precio = precio;
     }
-    set setReferencia(referencia){
+    set setReferencia(referencia) {
         this.referencia = referencia;
     }
-    set setFecha(fecha){
+    set setFecha(fecha) {
         this.fecha = fecha;
     }
-    set setPrecio(precio){
+    set setPrecio(precio) {
         this.precio = precio;
     }
-    toString(){
-        return "Referencia: "+this.referencia+" ,Fecha:"+this.fecha+" ,Precio:"+this.precio;
+    toString() {
+        return "Referencia: " + this.referencia + " ,Fecha:" + this.fecha + " ,Precio:" + this.precio;
     }
 }
 
@@ -212,16 +216,17 @@ let persona1 = new Persona("Javi", "Ruiz Anaya", "654987321", "Sevilla");
 let persona2 = new Persona("Carlos", "Mesa Viera", "654987321", "Murcia");
 let persona3 = new Persona("Jorge", "Te Queremos", "666666666", "Brookling");
 
-let usuario1 = new Usuario("Kmevi32", "tipo acero", "carlos.mesa@adaits.com");
-let usuario2 = new Usuario("iQuiiCks","tipo planta","javier.ruiz@adaits.com");
-let usuario3 = new Usuario("ShuJorgito", "tipo Sprint", "jorge.maquinon@adaits.com");
+let usuario1 = new Usuario("Kmevi32", "tipo acero", "carlos.mesa@adaits.com", [c1, c2]);
+let usuario2 = new Usuario("iQuiiCks", "tipo planta", "javier.ruiz@adaits.com", [c1]);
+let usuario3 = new Usuario("ShuJorgito", "tipo Sprint", "jorge.maquinon@adaits.com", [c3, c2]);
 
-let promocion1 = new Promocion("20%", "31/12/2019", [c1,c2]);
+let promocion1 = new Promocion("20%", "31/12/2019", [c1, c2]);
 let promocion2 = new Promocion("60%", "01/01/2020", [c1]);
-let promocion3 = new Promocion("110%", "02/02/2020"[c3,c2]);
+let promocion3 = new Promocion("110%", "02/02/2020" [c3, c2]);
 
 let pedido1 = new Pedido(usuario1, "U0001", "15/05/2019", "15000");
 let pedido2 = new Pedido(usuario2, "U0002", "16/05/2019", "150000");
 let pedido3 = new Pedido(usuario3, "U0003", "17/05/2019", "37000");
 
 let listaCoches = [c1, c2, c3];
+let listaUsuarios = [usuario1, usuario2, usuario3];
--- js/funsitones.js
+++ js/funsitones.js
@@ -21,21 +21,34 @@ let filtrarModelo = modelo => console.log(listaCoches.filter(x => x.modelo === m
 
 document.addEventListener("DOMContentLoaded", event => {
     document.getElementById("marca").addEventListener("keyup", validaMarca);
-    document.getElementById("modelo").addEventListener("keyup", validaModelo(document.getElementById("modelo")));
-    document.getElementById("formBuscarMarca").addEventListener("submit", comprobarFormulario());
+    document.getElementById("modelo").addEventListener("keyup", validaModelo);
+    document.getElementById("formBuscarMarca").addEventListener("submit", comprobarFormulario1);
+    /**/
+    document.getElementById("marcaSimilar").addEventListener("keyup", validaMarcaSimilar);
+    document.getElementById("formulario2").addEventListener("submit", comprobarFormulario2);
+    /**/
+
+    document.getElementById("formulario3").addEventListener("submit", comprobarFormulario3);
+    /**/
+    document.getElementById("anno").addEventListener("keyup", validaMarca4);
+    document.getElementById("formulario4").addEventListener("submit", comprobarFormulario4);
 })
 
-function marcarInputCorrecto(input){
+function marcarInputCorrecto(input) {
     input.classList.remove("erroneo");
     input.classList.add("valido");
 }
 
-function marcarInputErroneo(input){
+function marcarInputErroneo(input) {
     input.classList.remove("valido");
     input.classList.add("erroneo");
 }
 
-function validaMarca(){
+/*
+ * formulario1
+ */
+
+function validaMarca() {
     let marca = document.getElementById("marca");
     let correcto = true;
     let divErrores = document.getElementById("divErrorMarca");
@@ -53,32 +66,210 @@ function validaMarca(){
         divErrores.appendChild(div);
     }
     correcto ? marcarInputCorrecto(marca) : marcarInputErroneo(marca);
+    return correcto;
 }
 
 
+function validaModelo() {
+    let modelo = document.getElementById("modelo");
+    let correcto = true;
+    let divErrores = document.getElementById("divErrorModelo");
+    divErrores.innerHTML = "";
+    if (modelo.value.match(/[^a-zA-Z\d\-]/g)) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = `Solo se permiten letras, números y "-".`;
+        divErrores.appendChild(div);
+    }
+    if (modelo.value.length < 1) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = `Este campo no puede dejarlo vacío.`;
+        divErrores.appendChild(div);
+    }
+    correcto ? marcarInputCorrecto(modelo) : marcarInputErroneo(modelo);
+    return correcto;
+}
+
+function comprobarFormulario1(event) {
+    event.preventDefault();
+    let marca = validaMarca();
+    let modelo = validaModelo();
+    let mostrar = document.getElementById("mostrarMarcaModelo");
+    mostrar.innerHTML = "";
+    if (marca && modelo) {
+        let coches = filtrarMarca(document.getElementById("marca").value, document.getElementById("modelo").value);
+        let div = document.createElement("div");
+        for (let coche of coches) {
+            div.innerHTML += coche.marca + " modelo " + coche.modelo + "\n";
+        }
+        mostrar.appendChild(div);
+    } else {
+
+    }
+}
+
+/*
+ * formulario2 
+ */
+
+function validaMarcaSimilar() {
+    let marca = document.getElementById("marcaSimilar");
+    let correcto = true;
+    let divErrores = document.getElementById("divErrorMarcaSimilar");
+    divErrores.innerHTML = "";
+    if (marca.value.match(/\d/g)) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = "No se permiten número en este campo.";
+        divErrores.appendChild(div);
+    }
+    if (marca.value.length < 1) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = "Este campo no puede dejarlo vacío.";
+        divErrores.appendChild(div);
+    }
+    correcto ? marcarInputCorrecto(marca) : marcarInputErroneo(marca);
+    return correcto;
+}
+
+function comprobarFormulario2(event) {
+    event.preventDefault();
+    let mostrar = document.getElementById("mostrarSimilar");
+    mostrar.innerHTML = ""
+    if (validaMarcaSimilar()) {
+        let coches = elementoSimilar(document.getElementById("marcaSimilar").value);
+        let div = document.createElement("div");
+        for (let coche of coches) {
+            div.innerHTML += coche.marca + " modelo " + coche.modelo;
+        }
+        console.log(coches)
+        mostrar.appendChild(div);
+    } else {
+
+    }
+}
+
+/*
+ * formulario3
+ */
+
+function validaMarcaSimilar() {
+    let marca = document.getElementById("marcaSimilar");
+    let correcto = true;
+    let divErrores = document.getElementById("divErrorMarcaSimilar");
+    divErrores.innerHTML = "";
+    if (marca.value.match(/\d/g)) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = "No se permiten número en este campo.";
+        divErrores.appendChild(div);
+    }
+    if (marca.value.length < 1) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = "Este campo no puede dejarlo vacío.";
+        divErrores.appendChild(div);
+    }
+    correcto ? marcarInputCorrecto(marca) : marcarInputErroneo(marca);
+    return correcto;
+}
+
+function comprobarFormulario2(event) {
+    event.preventDefault();
+    let mostrar = document.getElementById("mostrarSimilar");
+    mostrar.innerHTML = ""
+    if (validaMarcaSimilar()) {
+        let coches = elementoSimilar(document.getElementById("marcaSimilar").value);
+        let div = document.createElement("div");
+        for (let coche of coches) {
+            div.innerHTML += coche.marca + " modelo " + coche.modelo;
+        }
+        console.log(coches)
+        mostrar.appendChild(div);
+    } else {
+
+    }
+}
+
+function comprobarFormulario3(event) {
+    event.preventDefault();
+    let mostrar = document.getElementById("mostrarAsociados");
+    mostrar.innerHTML = "";
+    let usuario = elementosAsociadosUsuario(document.getElementById("nombreUsuario").value);
+    let div = document.createElement("div");
+    for (let coche of usuario[0].coches) {
+        div.innerHTML += coche.marca + " modelo " + coche.modelo;
+    }
+    mostrar.appendChild(div);
+
+}
+/*
+ * formulario4
+ */
+
+function comprobarFormulario4(event) {
+    event.preventDefault();
+    let mostrar = document.getElementById("mostrarMarca");
+    mostrar.innerHTML = "";
+    if (validaMarca4()) {
+        let coches = filtrarAnno(document.getElementById("anno").value);
+        let div = document.createElement("div");
+        for (let coche of coches) {
+            div.innerHTML += coche.marca + " modelo " + coche.modelo;
+        }
+        mostrar.appendChild(div);
+    } else {
+
+    }
+}
+
+function validaMarca4() {
+    let marca = document.getElementById("anno");
+    let correcto = true;
+    let divErrores = document.getElementById("divErrorMarca4");
+    divErrores.innerHTML = "";
+    if (marca.value.match(/[^\d]/g)) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = "No se permiten número en este campo.";
+        divErrores.appendChild(div);
+    }
+    if (marca.value.length < 1) {
+        correcto = false;
+        let div = document.createElement("div");
+        div.innerHTML = "Este campo no puede dejarlo vacío.";
+        divErrores.appendChild(div);
+    }
+    correcto ? marcarInputCorrecto(marca) : marcarInputErroneo(marca);
+    return correcto;
+}
 
 /* BUSQUEDA */
 document.getElementById("marca")
-let filtrarMarca = (marca,modelo) => console.log(listaCoches.filter(x => x.marca === marca).filter(x => x.modelo === modelo));
-let elementoSimilar = coche => console.log(listaCoches.filter(x => coche.marca === x.marca).filter(x => coche.modelo === x.modelo));
-let elementosAsociadosUsuario = usuario => console.log(usuario.getCoches());
-let filtrarAnno = anno => console.log(listaCoches.filter(x => x.anno === anno));
+let filtrarMarca = (marca, modelo) => listaCoches.filter(x => x.marca === marca).filter(x => x.modelo === modelo);
+let elementoSimilar = cadena => listaCoches.filter(x => x.marca.includes(cadena) || x.modelo.includes(cadena));
+let elementosAsociadosUsuario = nombre => listaUsuarios.filter(x => x.nombre_usuario === nombre);
+let filtrarAnno = anno => listaCoches.filter(x => x.anno === anno);
 
 
 /* EDICION */
-let anadirCoche = (marca,modelo,anno,km,combustible) => console.log(listaCoches.push(new Coche(marca,modelo,anno,km,combustible)));
-let editarCoche = (posicion,marca) => console.log(listaCoches[posicion].setMarca = marca);
+let anadirCoche = (marca, modelo, anno, km, combustible) => console.log(listaCoches.push(new Coche(marca, modelo, anno, km, combustible)));
+let editarCoche = (posicion, marca) => console.log(listaCoches[posicion].setMarca = marca);
 let eliminarCoche = posicion => listaCoches.splice(posicion);
 //Funcion validar Sprint 4
 
 /* GESTION */
-let obtenerElementos = console.log(listaCoches.filter(x => x.marca === "Seat").sort((a,b)=>a.precio-b.precio));
-let mejorValorado = console.log(listaCoches.filter(x => x.anno === "2019").sort((a,b)=> a.votos-b.votos)[0]);
+let obtenerElementos = () => listaCoches.filter(x => x.marca === "Seat").sort((a, b) => a.precio - b.precio);
+let mejorValorado = () => listaCoches.filter(x => x.anno === "2019").sort((a, b) => a.votos - b.votos)[0];
 let ordenarPrecio = ordenacion => console.log(ordenacion === "asc" ? a.precio - b.precio : b.precio - a.precio);
 let ordenarAnno = ordenacion => console.log(ordenacion === "asc" ? a.anno - b.anno : b.anno - a.anno);
 
-/* USUARIO */ 
+/* USUARIO */
 let votarCoche = coche => coche.votos++;
 let elementosVotados = usuario => console.log(getListaCochesVotados());
-let elementosSimilares = console.log(listaCoches.filter(x => x.marca === getListaCochesVotados().marca));
+let elementosSimilares = () => listaCoches.filter(x => x.marca === getListaCochesVotados().marca);
 let mostrarUsuarios = () => console.log(listaUsuarios);
+SimilarSimilarSimilar
+let div = document.createElement("div");
