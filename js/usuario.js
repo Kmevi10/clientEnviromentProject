195fd68fd9ec13fe4704bf3e509c8ca4c9976c37
Kmevi10 <carlosmesaviera@gmail.com>
2019-12-13 19:43:30 +0100

    votos

--- js/usuario.js
+++ js/usuario.js
@@ -1,8 +1,8 @@
-$(() => $("#votarCoche").submit(event => {
+$(() => $("#votarCoche").click(event => {
     event.preventDefault();
     listaUsuarios.filter(x => x.nombre_usuario === $("#usuarioQueVota").val()).length >= 1 &&
         listaCoches.filter(x => x.modelo === $("#cocheAVotar").val()).length >= 1 ?
         listaVotos.push(new Voto($("#usuarioQueVota").val(), $("#votoCoche").val(), $("#cocheAVotar").val())) :
-        $("divErrorUser").append("<div>No es posible encontara su usuario.</div>");
+        $("divErrorUser").append(`<div>No es posible encontara su usuario, voto = ${$("#votoCoche").val()}.</div>`);
     console.log(listaVotos);
 }));
